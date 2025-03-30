760bd609d452325b52c684b726d322ae9adeac87
PFBregon <bregon@gmail.com>
2025-03-30 23:03:20 +0200

    testRecord run ok

--- src/main/java/dev/patriciafb/bankacount/models/Account.java
+++ src/main/java/dev/patriciafb/bankacount/models/Account.java
@@ -2,19 +2,24 @@ package dev.patriciafb.bankacount.models;
 
 public class Account {
 
+    private double balance;
+    private int numberConsignments;
+
     public Account(double initialBalance, int initialConsignments) {
-        
+        this.balance = initialBalance;
+        this.numberConsignments = initialConsignments;
     }
 
     public void record(double amount) {
-      
+        this.balance += amount;
+        this.numberConsignments++;
     }
 
     public double getBalance() {
-        
+        return this.balance;
     }
 
     public int getNumberConsignments() {
-        
+        return numberConsignments;
     }
 }
--- src/test/java/dev/patriciafb/bankacount/models/AccountTest.java
+++ src/test/java/dev/patriciafb/bankacount/models/AccountTest.java
@@ -1,16 +1,19 @@
 package dev.patriciafb.bankacount.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+
 
 public class AccountTest {
     @Test
     void testRecord() {
         Account account = new Account(1000, 5);
-        account.record(500);
-        assertThat(account.getBalance(), is(closeTo(1500, 0.01)));
-        assertThat(account.numberConsignments, is(1));
+        
+        account.record(50.0);
+        assertEquals(1050.0, account.getBalance());
+        assertEquals(6, account.getNumberConsignments());
     }
+        
 
 }
