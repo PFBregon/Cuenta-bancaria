246348c2a97f14ab9393ebe339cc934e50947e4a
PFBregon <bregon@gmail.com>
2025-03-30 21:01:50 +0200

    spring config

---
+++ src/main/java/dev/patriciafb/bankacount/BankacountApplication.java
@@ -0,0 +1,13 @@
+package dev.patriciafb.bankacount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankacountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankacountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/patriciafb/bankacount/BankacountApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patriciafb.bankacount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankacountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
