d1a0269825c6d9fb6980f191acd8db83da2ed9b2
PFBregon <bregon@gmail.com>
2025-03-31 17:56:10 +0200

    testSavingAccount run ok

--- src/main/java/dev/patriciafb/bankacount/models/SavingsAccount.java
+++ src/main/java/dev/patriciafb/bankacount/models/SavingsAccount.java
@@ -4,6 +4,7 @@ public class SavingsAccount extends Account {
     private boolean isActive;
 
     public SavingsAccount(double initialBalance, double annualInterestRate) {
-       
+        super(initialBalance, annualInterestRate);
+        this.isActive = initialBalance >= 10000;
     }
 }
--- src/test/java/dev/patriciafb/bankacount/models/SavingAccount.java
+++ src/test/java/dev/patriciafb/bankacount/models/SavingAccount.java
@@ -4,10 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.Test;
 
 public class SavingAccount {
-@Test
-void testSavingsAccountInactive() {
-    SavingsAccount account = new SavingsAccount(5000, 5);
-    account.deposit(6000);
-    assertTrue(account.getBalance() >= 10000);
-}
+    @Test
+    public void testSavingsAccountInactive() { // Make the method public
+        SavingsAccount account = new SavingsAccount(5000, 5);
+        account.deposit(6000);
+        assertTrue(account.getBalance() >= 10000);
+    }
 }
